0491e3c57a90e8149d7dfb298ed3fc49dd0a3d06
Matthew Allen <6348110+imMatt@users.noreply.github.com>
2019-02-15 16:35:51 +1030

    Populated default state variable

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 
 export default class Home extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      city:-1,
+      categories:[],
+      cuisines:[],
+      establishments:[],
+      searchParameters:{
+        category:"",
+        cuisine:"",
+        rating:{
+          min:0,
+          max:5
+        },
+        cost:{
+          min:1,
+          max:4
+        }
+      }
+    };
+  }
+
+  componentDidMount(){
+
+  }
+
   render() {
     return (
       <p>Home</p>
